fb99a6cc6ae3216883c12ade77d3fafe63b4b46c
JB <jb@bms-engineering.lu>
2021-12-13 22:53:40 +0100

    Ajoutez des fichiers projet.

---
+++ BlazorApp1/Shared/WeatherForecast.cs
@@ -0,0 +1,20 @@
+using HelloStandard;
+
+namespace BlazorApp1.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        private void Test()
+        {
+            var test = new Class1();
+        }
+    }
+}
